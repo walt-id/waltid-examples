fc309d6e7471e91206616c409283fcc005fd3b2c
ahmed smiai <ahmedsmiai3@gmail.com>
2024-02-28 15:59:46 +0100

    removed unnecessary dependencies & tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,28 +30,11 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
-    testImplementation("io.kotest:kotest-assertions-json:5.7.2")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-
-    // https://mvnrepository.com/artifact/com.nimbusds/nimbus-jose-jwt
-    implementation("com.nimbusds:nimbus-jose-jwt:9.38-rc3")
-
-    implementation("com.github.multiformats:java-multibase:v1.1.1")
-
 
     implementation("id.walt.crypto:waltid-crypto:$waltid_version")
     implementation("id.walt.credentials:waltid-verifiable-credentials:$waltid_version")
     implementation("id.walt.did:waltid-did:$waltid_version")
-    implementation("id.walt:waltid-sdjwt-jvm:$waltid_version")
+    implementation("id.walt:waltid-sdjwt:$waltid_version")
     implementation("id.walt:waltid-openid4vc:$waltid_version")
-
 
 }
