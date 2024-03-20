bcebbbca9d76b543896dd3e0e6eaacfca895440c
ahmed smiai <ahmedsmiai3@gmail.com>
2024-03-20 11:48:42 +0100

    did web resolve

--- src/main/kotlin/did/resolve/web_method.kt
+++ src/main/kotlin/did/resolve/web_method.kt
@@ -1,4 +1,13 @@
 package did.resolve
 
-class web_method {
+import id.walt.did.dids.DidService
+
+suspend fun main(){
+    DidService.minimalInit()
+    val didDocumentResult = DidService.resolve("did:web:wallet.walt-test.cloud:wallet-api:registry:1234")
+    val document = didDocumentResult.getOrNull()
+    println(document)
+    val keyResult = DidService.resolveToKey("did:web:wallet.walt-test.cloud:wallet-api:registry:1234")
+    val key = keyResult.getOrNull()
+    println(key)
 }
