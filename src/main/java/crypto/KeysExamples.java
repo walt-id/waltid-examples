d206557a8df4c144b2e6da3f5d09b210454e1777
waltkb <68587968+waltkb@users.noreply.github.com>
2024-04-29 19:06:56 +0200

    Better messages in java examples

--- src/main/java/crypto/KeysExamples.java
+++ src/main/java/crypto/KeysExamples.java
@@ -20,12 +20,20 @@ public class KeysExamples {
     public static void signBlocking() {
         System.out.println("Generating key synchronous...");
         JWKKey k = JWKKey.Companion.generateBlocking(KeyType.Ed25519, null);
+        System.out.println("Sync generated key: " + k);
         System.out.println("Signing with key synchronous...");
         var signed = (byte[]) k.signRawBlocking(plaintext);
 
         System.out.println("Signed synchronous: " + Arrays.toString(signed));
 
-        verifyAsync(k, signed);
+        verifyAsync(k, signed, plaintext, "Test sync verification");
+
+        System.out.println("Test a verification failure...");
+        byte[] invalid = new byte[signed.length + 1];
+        System.arraycopy(signed, 0, invalid, 0, signed.length);
+        invalid[signed.length] = Integer.valueOf(1).byteValue();
+
+        verifyAsync(k, signed, invalid, "Test verification failure");
     }
 
     public static void signAsync() {
@@ -33,21 +41,20 @@ public class KeysExamples {
 
         // join Futures to make sure they execute even when the program terminates earlier
         JWKKey.Companion.generateAsync(KeyType.Ed25519, null).thenAccept(key -> {
+            System.out.println("Async generated key: " + key);
             System.out.println("Signing with key asynchronous...");
             key.signRawAsync(plaintext).thenAccept(signed -> {
                 System.out.println("Signed asynchronous: " + Arrays.toString((byte[]) signed));
 
-                verifyAsync(key, (byte[]) signed);
+                verifyAsync(key, (byte[]) signed, plaintext, "Test async verification");
             });
         });
     }
 
-    public static void verifyAsync(Key key, byte[] signed) {
-        System.out.println("Verifying...");
-
+    public static void verifyAsync(Key key, byte[] signed, byte[] plaintext, String message) {
         key.getPublicKeyAsync().thenAccept(publicKey -> {
             publicKey.verifyRawAsync(signed, plaintext).thenAccept(result -> {
-                System.out.println("Verified: " + result);
+                System.out.println("Verification result (" + message + "): " + result);
             }).join();
         }).join();
     }
@@ -55,12 +62,14 @@ public class KeysExamples {
     public static String exportKey() {
         // Other KeyTypes:
         var key2 = JWKKey.Companion.generateBlocking(KeyType.secp256r1, null);
+        System.out.println("Export key...");
         String jwkExport = key2.exportJWKBlocking();
         System.out.println("Different KeyType, exported: " + jwkExport);
         return jwkExport;
     }
 
     public static void importKey(String jwk) throws ExecutionException, InterruptedException {
+        System.out.println("Import key...");
         Result<JWKKey> keyImport = JWKKey.Companion.importJWKAsync(jwk).get();
         System.out.println("Import result: " + keyImport);
     }
