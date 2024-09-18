e78a23c55d48a5c360d9982faa6bcaa05cb66ad5
mikeplotean <mike@walt.id>
2024-09-18 14:09:07 +0300

    chore: remove waltid api dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ version = "0.0.1"
 object Versions {
     const val KOTLIN_VERSION = "2.0.20-RC" // also change 1 plugin
     const val WALTID_VERSION = "0.7.0"
-    const val WALTID_API_VERSION = "0.6.0"
 }
 
 tasks.withType<Test>().configureEach {
@@ -35,9 +34,6 @@ dependencies {
     // all walt.id dependencies (not required for this project)
     implementation("id.walt.mdoc-credentials:waltid-mdoc-credentials:${Versions.WALTID_VERSION}")
     implementation("id.walt:waltid-service-commons:${Versions.WALTID_VERSION}")
-    implementation("id.walt:waltid-verifier-api:${Versions.WALTID_API_VERSION}")
-    implementation("id.walt:waltid-issuer-api:${Versions.WALTID_API_VERSION}")
-    implementation("id.walt.reporting:waltid-reporting:${Versions.WALTID_API_VERSION}")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("org.jetbrains:annotations:24.1.0")
